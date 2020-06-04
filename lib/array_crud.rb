0372b0b4eb45742136f97b48f53c678786bd45f9
osagie obaretin <osagie.codes@gmail.com>
2020-06-04 10:14:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  create_an_array=["wow", "I", "am", "really", "learning"]
+  create_an_array=["I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
